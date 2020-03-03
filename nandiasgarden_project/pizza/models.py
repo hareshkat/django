5a129e5c01a3f8b79ef9e8511c7b3e9c693e0b42
Haresh.Katariya <haresh.katariya@ril.com>
2020-03-03 11:15:53 +0530

    Nanidas Garden- django Form project

--- nandiasgarden_project/pizza/models.py
+++ nandiasgarden_project/pizza/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Size(models.Model):
-	title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
 
-	def __str__(self):
-		return self.title
 
 class Pizza(models.Model):
-	topping1 = models.CharField(max_length=100)
-	topping2 = models.CharField(max_length=100)
-	size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    topping1 = models.CharField(max_length=100)
+    topping2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
