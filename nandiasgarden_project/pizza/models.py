3d0155db0bfcdf361ac722e9ac6c1cae8d4ed0f2
hareshkat <you@example.com>
2020-03-05 15:08:24 +0530

    RESTFULL django project

--- nandiasgarden_project/pizza/models.py
+++ nandiasgarden_project/pizza/models.py
@@ -10,3 +10,7 @@ class Pizza(models.Model):
     topping1 = models.CharField(max_length=100)
     topping2 = models.CharField(max_length=100)
     size = models.ForeignKey(Size,on_delete=models.CASCADE)
+
+    def __str__(self):
+    	string = self.topping1+' + ' + self.topping2+' + '+str(self.size)
+    	return string
